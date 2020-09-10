4eb72f611aba34bc6d9f57191bbfc566da1a3015
Rohit <rohit.tiwari968@gmail.com>
2020-09-10 12:13:49 +0530

    Name and Email

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -2,32 +2,33 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [Errors, setErrors] = useState(false);
-  const [Users, setUsers] = useState({});
+  const [Users, setUsers] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const res  = await fetch('https://jsonplaceholder.typicode.com/users')
+  useEffect(async () => {
+    const result = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await result.json();
+    setUsers(data);
+  }, []);
+  
+  
 
-      res.json()
-      .then(res => setUsers(res))
-      .catch(err => setErrors(true));
-    }
-
-    fetchData();
-  });
-
-  const data = () => {
-    for (let user of Users) {
-      return {
-        (user.name),
-        (user.email)
-      }
-  }};
+  //const data = () => {
+   // for (let user of Users) {
+    //  return {
+      //  (user.name),
+        //(user.email)
+      //}
+  //}};
 
   return(
     <div>
-      <h1>{data.name}</h1>
-      <hr />
+      {Users.map((user) => (
+        <div key={user.id}>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <hr />
+        </div>
+      ))}
       <span>Has Error: {JSON.stringify(Errors)}</span>
     </div>
   );
